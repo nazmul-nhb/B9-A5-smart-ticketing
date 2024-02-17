1090d3d4782bd2055c84a5de41b5be0348003b7e
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-02-17 23:15:37 +0600

    banner section completed

---
+++ scripts/ticket.js
@@ -0,0 +1,8 @@
+// Buy Tickets Button in Banner Section
+function jumpToReference(){
+    const reference = document.getElementById('booking-section');
+    if(reference){
+        reference.scrollIntoView({behavior: 'smooth'});
+    }
+}
+
